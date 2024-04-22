38e199fa479e9f8455bc1fc1aac89516315e0fc9
nimai <nimai.m.patel@gmail.com>
2024-04-22 13:33:58 -0400

    add tests

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include "heap.h"
 
 int cmp(void *a, void *b) {
@@ -6,7 +7,7 @@ int cmp(void *a, void *b) {
 	return (*aa - *bb);
 }
 
-int check_heap(int *data, size_t len) {
+int heap_check(int *data, size_t len) {
 	for (size_t i = 0; i < len; i += 1) {
 		size_t left = i * 2 + 1;
 		size_t right = left + 1;
@@ -22,10 +23,69 @@ int check_heap(int *data, size_t len) {
 	return 1;
 }
 
-int main(void) {
+void arr_print(int *arr, size_t len) {
+	if (len == 0) {
+		printf("{}");
+	}
+
+	printf("{ ");
+	for (size_t i = 0; i < len - 1; i += 1) {
+		printf("%d ", arr[i]);
+	}
+	printf("%d }", arr[len - 1]);
+	printf("\n");
+}
+
+void test_build_heap(void) {
 	int heap[] = {-10, 3, 30, 100, 9, -11, 43, 3, -1};
 	size_t len = sizeof(heap) / sizeof(heap[0]);
 	build_heap(heap, len, cmp, sizeof(int));
-	printf("%d\n", check_heap(heap, len));
+	assert(heap_check(heap, len) == true);
+	printf("Passed %s...\n", __func__);
+}
+
+void test_decrease_key(void) {
+	int heap[] = {-10, 3, 30, 100, 9, -11, 43, 3, -1};
+	size_t len = sizeof(heap) / sizeof(heap[0]);
+	arr_print(heap, len);
+
+	build_heap(heap, len, cmp, sizeof(int));
+	arr_print(heap, len);
+	assert(heap_check(heap, len) == true);
+
+	int new_item = -1000;
+	size_t index = 4;
+	assert(new_item < heap[index]);
+	heap_update_key(heap, len, index, &new_item, cmp, sizeof(int));
+	arr_print(heap, len);
+	assert(heap_check(heap, len) == true);
+
+	printf("Passed %s...\n", __func__);
+}
+
+void test_increase_key(void) {
+	int heap[] = {-10, 3, 30, 100, 9, -11, 43, 3, -1};
+	size_t len = sizeof(heap) / sizeof(heap[0]);
+	arr_print(heap, len);
+
+	build_heap(heap, len, cmp, sizeof(int));
+	arr_print(heap, len);
+	assert(heap_check(heap, len) == true);
+
+	int new_item = 69;
+	size_t index = 4;
+	assert(new_item > heap[index]);
+	heap_update_key(heap, len, index, &new_item, cmp, sizeof(int));
+	arr_print(heap, len);
+	assert(heap_check(heap, len) == true);
+
+	printf("Passed %s...\n", __func__);
+}
+
+int main(void) {
+	test_build_heap();
+	test_decrease_key();
+	test_increase_key();
+	printf("Passed all tests...\n");
 	return 0;
 }
