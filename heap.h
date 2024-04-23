5bf2706c7ab791a7b6626e9ae9c81943a87210e0
nimai <nimai.m.patel@gmail.com>
2024-04-23 11:17:29 -0400

    refactor variable names

--- heap.h
+++ heap.h
@@ -4,37 +4,37 @@
 
 typedef int (*cmp_t) (void *a, void *b);
 
-void swap(void *a, void *b, size_t unit_size) {
-	uint8_t tmp[unit_size];
-	memcpy(tmp, a, unit_size);
-	memcpy(a, b, unit_size);
-	memcpy(b, tmp, unit_size);
+void swap(void *a, void *b, size_t width) {
+	uint8_t tmp[width];
+	memcpy(tmp, a, width);
+	memcpy(a, b, width);
+	memcpy(b, tmp, width);
 }
 
-void heapify_up(void *data, size_t index, const cmp_t cmp, const size_t unit_size) {
+void heapify_up(void *data, size_t index, const cmp_t cmp, const size_t width) {
 	while (index > 0) {
-		size_t parent_index = (index - 1)/ 2;
-		void *p_data = (uint8_t *)data + (unit_size * parent_index);
-		void *c_data = (uint8_t *)data + (unit_size * index);
+		size_t parent = (index - 1)/ 2;
+		void *p_data = (uint8_t *)data + (width * parent);
+		void *c_data = (uint8_t *)data + (width * index);
 		if (cmp(p_data, c_data) < 0) {
-			swap(p_data, c_data, unit_size);
-			index = parent_index;
+			swap(p_data, c_data, width);
+			index = parent;
 		} else {
 			return;
 		}
 	}
 }
 
-void heapify_down(void *data, const size_t len, size_t index, const cmp_t cmp, const size_t unit_size) {
+void heapify_down(void *data, const size_t len, size_t index, const cmp_t cmp, const size_t width) {
 	while (true) {
 		size_t left = (2 * index) + 1;
-		void *left_data = (uint8_t *)data + (unit_size * left);
+		void *left_data = (uint8_t *)data + (width * left);
 
 		size_t right = left + 1;
-		void *right_data = (uint8_t *)data + (unit_size * right);
+		void *right_data = (uint8_t *)data + (width * right);
 
 		size_t largest = index;
-		void *largest_data = (uint8_t *)data + (unit_size * largest);
+		void *largest_data = (uint8_t *)data + (width * largest);
 
 		if (left < len && cmp(left_data, largest_data) > 0) {
 			largest = left;
@@ -47,8 +47,8 @@ void heapify_down(void *data, const size_t len, size_t index, const cmp_t cmp, c
 		}
 
 		if (largest != index) {
-			void *index_data = (uint8_t *)data + unit_size * index;
-			swap(index_data, largest_data, unit_size);
+			void *index_data = (uint8_t *)data + width * index;
+			swap(index_data, largest_data, width);
 			index = largest;
 		} else {
 			break;
@@ -56,23 +56,23 @@ void heapify_down(void *data, const size_t len, size_t index, const cmp_t cmp, c
 	}
 }
 
-void heap_update_key(void *data, size_t len, size_t index, void *new_item, cmp_t cmp, size_t unit_size) {
-	void *old_item = (uint8_t *)data + unit_size * index;
+void heap_update_key(void *data, size_t len, size_t index, void *new_item, cmp_t cmp, size_t width) {
+	void *old_item = (uint8_t *)data + width * index;
 	int comp = cmp(new_item, old_item);
 
-	memcpy(old_item, new_item, unit_size);
+	memcpy(old_item, new_item, width);
 
 	if (comp > 0) {
-		heapify_up(data, index, cmp, unit_size);
+		heapify_up(data, index, cmp, width);
 	}
 
 	if (comp < 0) {
-		heapify_down(data, len, index, cmp, unit_size);
+		heapify_down(data, len, index, cmp, width);
 	}
 }
 
-void build_heap(void *data, const size_t len, const cmp_t cmp, const size_t unit_size) {
+void build_heap(void *data, const size_t len, const cmp_t cmp, const size_t width) {
 	for (size_t i = len / 2 - 1; i <= len / 2 - 1; i -= 1) {
-		heapify_down(data, len, i, cmp, unit_size);
+		heapify_down(data, len, i, cmp, width);
 	}
 }
